a952c4a4ba9eaf60a147b9fafd70210bfb9c0330
Daniel Eicher <d.eicher@mailbox.org>
2022-01-24 20:08:43 +0100

    Add a show answer/hide button

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,23 @@
 console.clear();
 
+// Bookmark Button
+
 const btnBookmark = document.querySelector('[data-js="btnBookmark"]');
 const imgBookmark = document.querySelector('[data-js="imgBookmark"]');
 
 btnBookmark.addEventListener("click", () => {
   imgBookmark.classList.toggle("card__bookmark--filled");
+});
+
+// Answer Button
+
+const btnAnswer = document.querySelector('[data-js="btnAnswer"]');
+const txtShow = document.querySelector('[data-js="txtShow"]');
+const txtHide = document.querySelector('[data-js="txtHide"]');
+const txtAnswer = document.querySelector('[data-js="txtAnswer"]');
+
+btnAnswer.addEventListener("click", () => {
+  txtAnswer.classList.toggle("card__hide");
+  txtShow.classList.toggle("card__hide");
+  txtHide.classList.toggle("card__hide");
 });
