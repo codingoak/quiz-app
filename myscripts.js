6c5feda29f5cd897622bb3329392570a5ef4904d
Daniel Eicher <d.eicher@mailbox.org>
2022-01-27 17:52:46 +0100

    Change answer button and navbar functions

--- myscripts.js
+++ myscripts.js
@@ -12,12 +12,49 @@ btnBookmark.addEventListener('click', () => {
 // Answer Button
 
 const btnAnswer = document.querySelector('[data-js="btnAnswer"]');
-const txtShow = document.querySelector('[data-js="txtShow"]');
-const txtHide = document.querySelector('[data-js="txtHide"]');
 const txtAnswer = document.querySelector('[data-js="txtAnswer"]');
 
-btnAnswer.addEventListener('click', () => {
+btnAnswer?.addEventListener('click', () => {
+  btnAnswer.textContent =
+    btnAnswer.textContent === 'SHOW ANSWER' ? 'HIDE ANSWER' : 'SHOW ANSWER';
   txtAnswer.classList.toggle('card__hide');
-  txtShow.classList.toggle('card__hide');
-  txtHide.classList.toggle('card__hide');
+});
+
+// Navbar
+
+const btnHome = document.querySelector('[data-js="home"]');
+const btnBookmarks = document.querySelector('[data-js="bookmarks"]');
+const btnCreate = document.querySelector('[data-js="create"]');
+const btnProfile = document.querySelector('[data-js="profile"]');
+
+btnHome.addEventListener('click', () => {
+  console.log('home clicked');
+  btnHome.classList.add('nav__active');
+  btnBookmarks.classList.remove('nav__active');
+  btnCreate.classList.remove('nav__active');
+  btnProfile.classList.remove('nav__active');
+});
+
+btnBookmarks.addEventListener('click', () => {
+  console.log('bookmarks clicked');
+  btnBookmarks.classList.add('nav__active');
+  btnHome.classList.remove('nav__active');
+  btnCreate.classList.remove('nav__active');
+  btnProfile.classList.remove('nav__active');
+});
+
+btnCreate.addEventListener('click', () => {
+  console.log('create clicked');
+  btnCreate.classList.add('nav__active');
+  btnHome.classList.remove('nav__active');
+  btnBookmarks.classList.remove('nav__active');
+  btnProfile.classList.remove('nav__active');
+});
+
+btnProfile.addEventListener('click', () => {
+  console.log('profile clicked');
+  btnProfile.classList.add('nav__active');
+  btnHome.classList.remove('nav__active');
+  btnBookmarks.classList.remove('nav__active');
+  btnCreate.classList.remove('nav__active');
 });
